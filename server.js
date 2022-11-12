6c0b4bd3096ccbf12ef812aec97f131a2eaa5dd0
ali <ali.rafique026@gmail.com>
2022-11-12 13:46:20 +0530

    All views added

--- server.js
+++ server.js
@@ -24,6 +24,72 @@ app.get('/',async (req, res) => {
   res.render('home')
 })
 
+app.get('/index',async (req, res) => {
+	res.render('home')
+  })
+
+// Account Page
+app.get('/account',async (req, res) => {
+	res.render('account')
+  })
+
+// Collection Page
+app.get('/collection',async (req, res) => {
+	res.render('collection')
+  })
+
+
+// Contact Page
+app.get('/contact',async (req, res) => {
+	res.render('contact')
+  })
+
+// Create-account Page
+app.get('/create-account',async (req, res) => {
+	res.render('create-account')
+  })
+
+
+// Login Page
+app.get('/login',async (req, res) => {
+	res.render('login')
+  })
+
+// Product Page
+app.get('/product',async (req, res) => {
+	res.render('product')
+  })
+
+// Shop Page
+app.get('/shop',async (req, res) => {
+	res.render('shop')
+  })
+
+// Single Page
+app.get('/single',async (req, res) => {
+	res.render('single')
+  })
+
+// Cart Page
+app.get('/cart',async (req, res) => {
+	res.render('cart')
+  })
+
+// Collection Page
+app.get('/collection',async (req, res) => {
+	res.render('collection')
+  })
+
+  // Contact Page
+app.get('/contact',async (req, res) => {
+	res.render('contact')
+  })
+
+// Blog Page
+app.get('/blog',async (req, res) => {
+	res.render('blog')
+  })
+
 // For error page
 app.get("*",function(req,res)
 	{
